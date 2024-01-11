7a2a26febb4d757d1299df3201b9309ebda3348b
Snezhana <snega.chabanenko@gmail.com>
2024-01-11 11:28:55 +0200

    newemail.js

---
+++ newletter.js
@@ -0,0 +1,11 @@
+// Regular expression for email validation
+var emailRegex = /^[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*@(gmail\.com|yahoo\.com)$/;
+
+// Validation and filtering of the array
+var trustedEmails = arr.filter(function(obj) {
+    return emailRegex.test(obj.email);
+}).map(function(obj) {
+    return obj.email;
+});
+
+console.log(trustedEmails);
